b1b48a8d5bd210acc5e78da8089e5267193c473c
DiegoBeja <diego.bejarano@uabc.edu.mx>
2025-02-18 20:43:47 -0800

    tarea2-2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Ejemplo x = new Ejemplo();
+
+//        System.out.println(x.invertirCadena("Hola mundo"));
+//
+//        System.out.println(x.revisarSintaxis("{[()]}"));
+//        System.out.println(x.revisarSintaxis("{[)]}"));
+//        System.out.println(x.revisarSintaxis("{[()]"));
+
         int [] numeros = {5,2,1,9,4,2};
 
         Pila<Integer> resultado = x.menorAMayor(numeros);
 
-        // Imprimir el resultado
-        while (!resultado.pilaVacia()) {
+        while(!resultado.pilaVacia()){
             System.out.println(resultado.pop());
         }
     }
